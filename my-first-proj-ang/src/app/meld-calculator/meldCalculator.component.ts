f65b7877aa5c14108f99f15ff988d8581f2da1dd
Bipin Krishnan <bipinkris@gmail.com>
2022-11-03 08:01:34 +0530

    Initial Version - MELD Calculator

---
+++ my-first-proj-ang/src/app/meld-calculator/meldCalculator.component.ts
@@ -0,0 +1,13 @@
+import { Component } from "@angular/core";
+
+//Logic
+@Component({
+    templateUrl:'./meldCalculator.component.html',
+    selector:'meld-calc'
+})
+export class MeldCalculatorComponent{
+
+    constructor(){
+    }
+
+}
